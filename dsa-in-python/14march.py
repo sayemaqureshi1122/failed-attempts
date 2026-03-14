4c93d1f8fdecf997a5e1db5d7ca2e288bee5a2a9
Sayema Qureshi <qureshisayema47@gmail.com>
2026-03-14 07:00:54 +0530

    using bitwise logic

--- dsa-in-python/14march.py
+++ dsa-in-python/14march.py
@@ -1,5 +1,25 @@
-a = input()
-b = input()
-c = input()
+# a = input()
+# b = input()
+# c = input()
 
-print(str(a)*a)
+# print(str(a)*a)
+
+# # new way of finding even or odd (but only works with if the input is integer)
+# a = int(input())
+# if a & 1:
+#     print("odd")
+# else:
+#     print("even")
+
+#new way to swap numbers without using built-in function, temp var
+a =int(input())
+b = int(input())
+
+# to swap
+a1 = a ^ b
+print(a1)
+b1 = a1 ^ b
+print(b1)
+a2 = b1 ^ a1
+print(a2, b1)
+#got the ans as both the numbers are swapped 
