36ccca649bd07cb09f023facd34717f9ece271b3
Sayema Qureshi <qureshisayema47@gmail.com>
2026-03-16 06:30:02 +0530

    notes on conditional statements

---
+++ dsa-in-python/16march.py
@@ -0,0 +1,18 @@
+''' if-elif-else statement - >
+    1] greater than - >
+    2] less than - <
+    3] greater than equal - >=
+    4] less than equal - <=
+    5] assignment - =
+    6] equal - ==
+    7] strictly equal - ===
+    8] is - this one checks that if it is the same object or not that means it checks 
+    (id(a) == id(b)) cuz each obj stored in the mem has different id and that's how it differs from ==.
+    
+    
+    False values:
+        False - boolean
+        None
+        Zero
+        empty sequence - "" -> empty str, () -> empty tuple, [] -> empty list, {} -> empty dict.
+'''
