8a36b2e2d2bdeca8c1db270c25a2502a56efa480
Sayema Qureshi <qureshisayema47@gmail.com>
2026-03-27 22:58:55 +0530

    questions

--- dsa-in-python/27march.py
+++ dsa-in-python/27march.py
@@ -46,36 +46,80 @@ Set methods -- > cannot store duplicate values
 4] set(set1) - to create an empty set cuz set1 = {} becomes a dict
 '''
 
-courses = ["maths", "bio", "chem", "physics", "english"]
-courses.insert(1, "art")
-courses.remove("art")
-courses.pop()
-courses2 = ["art", "sports", "design"]
-courses.extend(courses2)
-courses.sort()
-courses.reverse()
-sorted_courses = sorted(courses)
-print(sorted_courses)
-print("bio" in courses)
-print(sorted_courses.index("maths"))
-h1 = list(enumerate(sorted_courses))
-print(h1)
-h2 = enumerate(sorted_courses)
-print(next(h2)) 
+# courses = ["maths", "bio", "chem", "physics", "english"]
+# courses.insert(1, "art")
+# courses.remove("art")
+# courses.pop()
+# courses2 = ["art", "sports", "design"]
+# courses.extend(courses2)
+# courses.sort()
+# courses.reverse()
+# sorted_courses = sorted(courses)
+# print(sorted_courses)
+# print("bio" in courses)
+# print(sorted_courses.index("maths"))
+# h1 = list(enumerate(sorted_courses))
+# print(h1)
+# h2 = enumerate(sorted_courses)
+# print(next(h2)) 
 
 
-tuple1 = (1, 2, 3, 4)
-l1 = list(tuple1)
-l1.insert(0, 0)
-tuple2 = tuple(l1)
+# tuple1 = (1, 2, 3, 4)
+# l1 = list(tuple1)
+# l1.insert(0, 0)
+# tuple2 = tuple(l1)
 
-print(tuple1)
-print(tuple2)
+# print(tuple1)
+# print(tuple2)
 
 
-set1 = {"math", "science", "sports"}
-set2 = {"art", "design", "math"}
-print(set1.intersection(set2))
-print(set1.difference(set2)) # set 1 ke reference me
-print(set2.difference(set1))
-print(set1.union(set2))
+# set1 = {"math", "science", "sports"}
+# set2 = {"art", "design", "math"}
+# print(set1.intersection(set2))
+# print(set1.difference(set2)) # set 1 ke reference me
+# print(set2.difference(set1))
+# print(set1.union(set2))
+
+
+'''Q1] 1️⃣ Find Maximum '''
+# nums = list(map(int,input("enter list: ").split()))
+# def find_max(nums):
+#     maximum = nums[0]
+#     for i in range(len(nums)-1):
+#         if nums[i] > max:
+#             maximum = nums[i]
+#     return maximum
+# res = find_max(nums)
+# print(res)
+
+'''Q2] 1️⃣ Find Maximum '''
+nums = list(map(int,input("enter list: ").split()))
+# def find_min(nums):
+#     minimum = nums[0]
+#     for i in range(len(nums)-1):
+#         if nums[i] < minimum:
+#             minimum = nums[i]
+#     return minimum
+# res = find_min(nums)
+# print(res)
+            
+'''3️⃣ Second Largest (IMPORTANT)'''
+def second_largest(nums):
+    largest = nums[0]
+    second_largest = nums[0]
+    for i in range(len(nums)-1):
+        if nums[i] > largest:
+            largest = nums[i]
+        
+    for j in range(len(nums)-1):
+        if nums[j] > second_largest and nums[j] < largest:
+            second_largest = nums[j]
+    return second_largest ,largest
+res = second_largest(nums)
+print(res)
+
+       
+    
+
+
+
