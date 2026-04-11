4707180e01d2701856db8876c474b12f1cd7312d
Sayema Qureshi <qureshisayema47@gmail.com>
2026-04-11 20:53:21 +0530

    day6 done

---
+++ python_codewithharry/var_datatypes.py
@@ -0,0 +1,18 @@
+# everything thing / data/ function in python is an object.
+
+a = 1 #numeric
+b = complex(4, 2) #numeric - int
+c = "hello" #text - str
+d = True #boolean 
+e = 2.4553 #numeric - float
+f = [1, 2, 3,[4, 5]] # sequenced - list
+g = (1, 2, 4, 5) #sequenced - tuple
+h = {"name" : "sayema"} #mapped - dict
+print(f"type of a is {type(a)}")
+print(f"type of b is {type(b)}")
+print(f"type of c is {type(c)}")
+print(f"type of d is {type(d)}")
+print(f"type of e is {type(e)}")
+print(f"type of f is {type(f)}")
+print(f"type of g is {type(g)}")
+print(f"type of h is {type(h)}")
